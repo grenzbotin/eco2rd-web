24db7c6ff02703dfb0b27edaf9f589f3d462d778
grenzbotin <grenzbotin@googlemail.com>
2022-06-26 09:33:37 +0200

    visual improvements

--- config/extension.ts
+++ config/extension.ts
@@ -2,7 +2,7 @@ const EXTENSION_STATUS = [
   {
     id: 6,
     date: new Date(2022, 5, 25),
-    title: "🏛 v0.1.4 - Historical data view update",
+    title: "🏛 v0.1.4 - Historical data",
     link: "https://github.com/grenzbotin/eco2rd/releases/tag/v0.1.4",
     version: "v0.1.4",
     content: "Historical data can now be found in the 'historical' tab that allows to compare data transfer, co2eq and electricity usage over months and years. Note: Historical data will start to be recorded with the installation of the update.",
